5a532d81af97e2d4f9feaea9c6497d16463a3a76
Guocheng Qian <guocheng.qian@outlook.com>
2020-12-03 09:38:16 +0300

    env install

--- deepgcn_env_install.sh
+++ deepgcn_env_install.sh
@@ -27,10 +27,11 @@ CUDA=cu100
 pip install torch-scatter==2.0.4 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
 pip install torch-sparse==0.6.1 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
 pip install torch-spline-conv==1.2.0 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
-pip install torch-cluster==1.4.5
+pip install torch-cluster==1.4.5 -f https://pytorch-geometric.com/whl/torch-1.4.0+${CUDA}.html
 pip install torch-geometric==1.4.3
 pip install requests
 
 # install useful modules
 pip install tqdm
+
 
