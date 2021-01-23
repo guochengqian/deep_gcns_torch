4ea499af7e44e26777dfeede74a0455ec63ba125
Guocheng Qian <guocheng.qian@outlook.com>
2021-01-23 17:04:57 +0300

    gradient clipping

--- examples/ogb/ogbg_mol/args.py
+++ examples/ogb/ogbg_mol/args.py
@@ -30,6 +30,9 @@ class ArgsInit(object):
         parser.add_argument('--lr', type=float, default=0.01,
                             help='learning rate set for optimizer.')
         parser.add_argument('--dropout', type=float, default=0.5)
+        parser.add_argument('--grad_clip', type=float, default=0.,
+                            help='gradient clipping')
+
         # model
         parser.add_argument('--num_layers', type=int, default=3,
                             help='the number of layers of the networks')
@@ -54,6 +57,10 @@ class ArgsInit(object):
                             help='the power of PowerMean')
         parser.add_argument('--learn_t', action='store_true')
         parser.add_argument('--learn_p', action='store_true')
+        parser.add_argument('--y', type=float, default=0.0,
+                            help='the power of softmax_sum and powermean_sum')
+        parser.add_argument('--learn_y', action='store_true')
+
         # message norm
         parser.add_argument('--msg_norm', action='store_true')
         parser.add_argument('--learn_msg_scale', action='store_true')
@@ -74,11 +81,12 @@ class ArgsInit(object):
 
     def save_exp(self):
         self.args.save = '{}-B_{}-C_{}-L_{}-F_{}-DP_{}' \
-                    '-GA_{}-T_{}-LT_{}-P_{}-LP_{}' \
+                    '-GA_{}-T_{}-LT_{}-P_{}-LP_{}-Y_{}-LY_{}' \
                     '-MN_{}-LS_{}'.format(self.args.save, self.args.block, self.args.conv,
                                           self.args.num_layers, self.args.hidden_channels,
                                           self.args.dropout, self.args.gcn_aggr,
                                           self.args.t, self.args.learn_t, self.args.p, self.args.learn_p,
+                                          self.args.y, self.args.learn_y,
                                           self.args.msg_norm, self.args.learn_msg_scale)
 
         self.args.save = 'log/{}-{}-{}'.format(self.args.save, time.strftime("%Y%m%d-%H%M%S"), str(uuid.uuid4()))
